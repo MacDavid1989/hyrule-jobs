1a587bdab799b06f36f5c65ac84e2b163fd01965
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-29 11:25:20 -0400

    use some basic functions and refs to show how ts interacts with the composition api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
-<template>
-  <div class="app">hello, hyrule</div>
-</template>
-
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    // name
+    const name = ref<string>("Link");
+    const changeName = (newName: string): void => {
+      name.value = newName;
+    };
+
+    // age
+    const age = ref<number | string>(25);
+    const changeAge = (newAge: number | string): void => {
+      age.value = newAge;
+    };
+
+    return { name, changeName, age, changeAge };
+  },
 });
 </script>
+
+<template>
+  <div class="app">
+    <p>
+      {{ name }}
+      -
+      {{ age }}
+    </p>
+    <button @click="changeName('Zelda')">Change Name</button>
+    <button @click="changeAge(30)">Change Name</button>
+  </div>
+</template>
 
 <style></style>
